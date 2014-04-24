f6866784885d3851c3b3bd9e3e5db825c5720858
Stijn Haulotte <stijnhau@gmail.com>
2014-04-24 21:13:26 +0200

    Module file and begin of controller.

---
+++ Module.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace ZfcUserList;
+
+use Zend\ModuleManager\Feature\ServiceProviderInterface;
+
+
+class Module implements ServiceProviderInterface
+{
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getServiceConfig()
+    {
+        return include __DIR__ . '/config/services.config.php';
+    }
+}
---
+++ src/ZfcUserList/Controller/UserListController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace ZfcUserList\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\Paginator;
+use ZfcUser\Mapper\UserInterface;
+use ZfcUser\Options\ModuleOptions as ZfcUserModuleOptions;
+use ZfcUserAdmin\Options\ModuleOptions;
+
+class UserListController extends AbstractActionController
+{
+    protected $options, $userMapper;
+    protected $zfcUserOptions;
+    /**
+     * @var \ZfcUserAdmin\Service\User
+     */
+    protected $adminUserService;
+
+    public function listAction()
+    {
+        $userMapper = $this->getUserMapper();
+        $users = $userMapper->findAll();
+        if (is_array($users)) {
+            $paginator = new Paginator\Paginator(new Paginator\Adapter\ArrayAdapter($users));
+        } else {
+            $paginator = $users;
+        }
+
+        $paginator->setItemCountPerPage(100);
+        $paginator->setCurrentPageNumber($this->getEvent()->getRouteMatch()->getParam('p'));
+        return array(
+            'users' => $paginator,
+            'userlistElements' => $this->getOptions()->getUserListElements()
+        );
+    }
+
+    public function setOptions(ModuleOptions $options)
+    {
+        $this->options = $options;
+        return $this;
+    }
+
+    public function getOptions()
+    {
+        if (!$this->options instanceof ModuleOptions) {
+            $this->setOptions($this->getServiceLocator()->get('zfcuseradmin_module_options'));
+        }
+        return $this->options;
+    }
+
+    public function getUserMapper()
+    {
+        if (null === $this->userMapper) {
+            $this->userMapper = $this->getServiceLocator()->get('zfcuser_user_mapper');
+        }
+        return $this->userMapper;
+    }
+
+    public function setUserMapper(UserInterface $userMapper)
+    {
+        $this->userMapper = $userMapper;
+        return $this;
+    }
+    public function setZfcUserOptions(ZfcUserModuleOptions $options)
+    {
+        $this->zfcUserOptions = $options;
+        return $this;
+    }
+
+    /**
+     * @return \ZfcUser\Options\ModuleOptions
+     */
+    public function getZfcUserOptions()
+    {
+        if (!$this->zfcUserOptions instanceof ZfcUserModuleOptions) {
+            $this->setZfcUserOptions($this->getServiceLocator()->get('zfcuser_module_options'));
+        }
+        return $this->zfcUserOptions;
+    }
+}
