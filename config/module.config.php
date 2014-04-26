659707e96ab4d630ba45c9511d67cee3bb238dda
Stijn Haulotte <stijnhau@gmail.com>
2014-04-26 10:12:30 +0200

    added routes + updated needed php version

--- config/module.config.php
+++ config/module.config.php
@@ -1,1 +1,31 @@
 <?php
+return array(
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'zfcuserlist' => __DIR__ . '/../view',
+        ),
+    ),
+    'controllers' => array(
+        'invokables' => array(
+            'zfcuserlist' => 'ZfcUserAdmin\Controller\UserListController',
+        ),
+    ),
+    'router' => array(
+        'routes' => array(
+            'zfcuser' => [
+                'child_routes' => [
+                    'zfcuserlist' => [
+                        'type' => 'Segment',
+                        'options' => [
+                            'route' => '/list',
+                            'defaults' => [
+                                'controller' => 'zfcuserlist',
+                                'action' => 'list'
+                            ]
+                        ]
+                    ],
+                ],
+            ],
+        ),
+    ),
+);
